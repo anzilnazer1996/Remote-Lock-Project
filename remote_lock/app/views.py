9112d1b9104e66d4b526c6c449538db783e7adc8
Anzil <anzilnazer1996@gmail.com>
2025-04-30 20:23:08 +0530

    decorator added

--- remote_lock/app/views.py
+++ remote_lock/app/views.py
@@ -38,6 +38,8 @@ from django.utils.decorators import method_decorator
 
 from django.contrib.auth.decorators import login_required
 
+from django.urls import reverse_lazy
+
 
 
 class LoginView(View):
